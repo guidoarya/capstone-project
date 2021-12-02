443b3a16c7ffacdc568dc24815c61b4454a8f753
wilantarajaya <61103666+wilantarajaya@users.noreply.github.com>
2021-12-02 13:36:09 +0800

    Menambhakan file front-end
    Menambahkan halam home

---
+++ front-end/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import axios from 'axios';
+import './style.css';
+import {} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+axios.defaults.withCredentials = true;
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
