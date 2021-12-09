34a8aad9c0bb74cb105a055708807ce3ea21de1f
wilantarajaya <61103666+wilantarajaya@users.noreply.github.com>
2021-12-09 21:49:05 +0800

    Update DetailUMKM
    Memperbaiki halaman DetailUMKM

--- src/components/page/DetailUMKM.js
+++ src/components/page/DetailUMKM.js
@@ -1,5 +1,6 @@
 import { Container, Image } from "react-bootstrap";
-import { Link, useHistory } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapPin, faThLarge } from "@fortawesome/free-solid-svg-icons";
 import "./DetailUMKM.css";
 
 const DetailUMKM = () => {
@@ -8,18 +9,24 @@ const DetailUMKM = () => {
       <Container>
         <div className="content-detail">
           <div className="title-umkm">
-            <h1>Detail UMKM</h1>
+            <h1>Nama UMKM</h1>
+            <h2>Jln. Bandung Indonesia</h2>
           </div>
           <div className="wrap-content-detail">
             <div className="image-detail">
+              <h3>
+                <FontAwesomeIcon icon={faThLarge} className="icon-map" /> Kategori
+              </h3>
               <Image className="image-detail-umkm" src="images/test.jpg"></Image>
             </div>
             <div className="info-umkm">
               <div className="umkm-title">
-                <h2>Nama UMKM</h2>
-                <h4>Lokasi</h4>
-                <h4>Kategori</h4>
+                <h4>
+                  <FontAwesomeIcon icon={faMapPin} className="icon-map" /> Lokasi
+                </h4>
                 <h3>Deskripsi</h3>
+                <p>lomemd dfndn feifieninfie feinfienfienf</p>
+                <h3>Item Dijual</h3>
                 <p>lomemd dfndn feifieninfie feinfienfienf</p>
               </div>
             </div>
